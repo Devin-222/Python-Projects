e10af5582ec1ec8265a73544aa010ba9c7106dbf
Devin Patel <devin.patel2004@gmail.com>
2025-06-05 10:58:40 +0100

    Create Calculator.py
    Simple calculator program.
    
    Will be updating with new features.

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+print("""Calculator:
+      +---------+
+      0
+      +---------+
+      | [+] [-] |
+      | [*] [/] |
+      +---------+
+      """)
+
+print("First number: 558.28")
+print("Operator: +")
+print("Second number: 452.64")
+
+print()
+
+print("Answer:")
+print("+---------+")
+print(558.28 + 452.64)
+print("+---------+")
