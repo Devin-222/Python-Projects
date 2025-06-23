3d0abed7128a4bd248ca20d5a2694097b764646a
Devin Patel <devin.patel2004@gmail.com>
2025-06-23 16:04:34 +0100

    Update Calculator.py
    Version 1.4.1
    
    - added the operators to a tuple and using that for the operator input messages

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,24 @@
+# While loop to allow the user to do another calculation if they choose to do so
 while True:
     # Calculator display
-    print("""Calculator:
-    +---------+
-    0
-    +---------+
-    | [+] [-] |
-    | [*] [/] |
-    +---------+
-    """)
+    print("""
+Calculator:
++---------+
+0
++---------+
+| [+] [-] |
+| [*] [/] |
++---------+
+""")
 
     # User input for the two numbers to be operated on and the operator to be used
     num1 = float(input("First number: "))
 
     # Simple operator validator that checks if the input from the user is a valid operator from the tuple
     valid_operators = ("+", "-", "*", "/")
-    operator = input("Enter operator: ")
+    operator = input(f"Enter operator{valid_operators}: ")
     while operator not in valid_operators:
-        print("Invalid operator, choose one of the following: +, -, *, /")
+        print(f"Invalid operator, choose one of the following{valid_operators}: ")
         operator = input("Enter operator: ")
     
     num2 = float(input("Second number: "))
